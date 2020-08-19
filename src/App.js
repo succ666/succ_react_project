34c5f22ad433decc142f8b0ad2ae780491e2a09e
tom_glb <goodMorning_glb@atguigu.com>
2020-08-19 11:40:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React,{Component} from 'react';
-import {Switch,Route} from 'react-router-dom';
-import Admin from './pages/admin/admin';
-import Login from './pages/login/login'
+import React,{Component} from "react"
+
 
 export default class App extends Component{
   render(){
     return(
-      <div className='app'>
-        <Switch>
-          <Route path="/admin" component={Admin}/>
-          <Route path="/login" component={Login}/>
-        </Switch>
-      </div>
+      <div>app</div>
     )
   }
 }
