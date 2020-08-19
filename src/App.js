f102264fe00c3c2c23961e7a6aea58fe7008e555
tom_glb <goodMorning_glb@atguigu.com>
2020-08-19 10:57:37 +0800

    day01 succ_react_project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import {Switch,Route} from 'react-router-dom';
+import Admin from './pages/admin/admin';
+import Login from './pages/login/login'
+
+export default class App extends Component{
+  render(){
+    return(
+      <div className='app'>
+        <Switch>
+          <Route path="/admin" component={Admin}/>
+          <Route path="/login" component={Login}/>
+        </Switch>
+      </div>
+    )
+  }
+}
